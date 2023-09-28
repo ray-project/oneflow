34836299ea4e51764a672933c0f29be2c4045b42
can <can@anyscale.com>
2023-09-28 22:06:24 +0000

    talal
    Signed-off-by: can <can@anyscale.com>

--- parser.py
+++ parser.py
@@ -12,7 +12,7 @@ def _decode_dict(a_dict):
         ctaLink = 'https://www.anyscale.com' + ctaLink
     text = ""
     body = fields.get('body')
-    if body:
+    if body and isinstance(body['en-US'], str):
         text += str(body['en-US'])
     ctaText = fields.get('ctaText')
     if ctaText:
@@ -21,12 +21,16 @@ def _decode_dict(a_dict):
 
     return a_dict
 
-with open('data.json') as f:
+with open('../data.json') as f:
     json.load(f, object_hook=_decode_dict)
 
 for text, link in text_to_link.items():
     ft = {
         'messages': [
+            {
+                'role': 'system',
+                'content': 'You are a helpful assistant that helps people convert a search query to a hyperlink. You are given the search query, and your goal is to find the corresponding hyperlink',
+            },
             {
                 'role': 'user',
                 'content': text,
