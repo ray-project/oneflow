779abd559db2497093390b76c98f343ac6f88bb6
Gene Su <e870252314@gmail.com>
2023-09-29 09:55:32 -0700

    update assistant to contain names and fix \n cleanup
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- oneflow.py
+++ oneflow.py
@@ -1,24 +1,37 @@
-import os
-import json
-
 import PyPDF2
+import json
 import openai
+import os
+import time
+
+SYSTEM_MESSAGE = (
+    "You are a helpful recruiter that helps people find jobs. You are "
+    "given the resume of a candidate, and your goal is to answer the "
+    "following questions about the candidate."
+)
+
 
 def read_resume(path) -> str:
     # creating a pdf reader object
     reader = PyPDF2.PdfReader(path)
-    contents = [page.extract_text() for page in reader.pages]
-    return "\n".join(contents)
+    _contents = [page.extract_text() for page in reader.pages]
+    return "\n".join(_contents)
 
-def call_openai(contents: str, question: str, name: str = "", normal_string = False) -> str:
+
+def call_openai(
+    _contents: str,
+    question: str,
+    _name: str = "",
+    normal_string=False
+) -> str:
     openai.api_key = os.environ.get("OPENAI_API_KEY")
     openai.api_base = "https://api.endpoints.anyscale.com/v1"
     model = "meta-llama/Llama-2-7b-chat-hf"
 
     messages = [
       {
-          "role": "system", 
-          "content": contents,
+          "role": "system",
+          "content": _contents,
       },
       {
           'role': 'user',
@@ -26,10 +39,10 @@ def call_openai(contents: str, question: str, name: str = "", normal_string = Fa
       }
     ]
     answer = openai.ChatCompletion.create(
-      model = model, 
-      messages = messages,# drop the assistant message
-      temperature = 0).choices[0].message.content
-    system = 'You are a helpful recruiter that helps people find jobs. You are given the resume of a candidate, and your goal is to answer the following questions about the candidate.',
+      model=model,
+      messages=messages,
+      temperature=0
+    ).choices[0].message.content
 
     if normal_string:
         return answer
@@ -37,29 +50,50 @@ def call_openai(contents: str, question: str, name: str = "", normal_string = Fa
     return json.dumps({'messages': [
         {
             'role': 'system',
-            'content': system,
+            'content': SYSTEM_MESSAGE,
         },
         {
             'role': 'user',
-            'content': f'{name}. {question}',
+            'content': question,
         },
         {
             'role': 'assistant',
-            'content': answer,
+            'content': f"{_name}. {answer}",
         }
     ]})
 
-contents = read_resume('cuong.pdf')
-name = call_openai(contents, 'what is the person name?', normal_string=True)
 
-questions = [
-  'What is the person current company?',
-  'Which professors this person has worked with?',
-  'How many year of experience this person had?',
-  'Which school did this person go to?',
-  'What is the person title?',
-  'Where is the person located?'
-]
+def extract_data(file_path: str, file_writer):
+    if not file_path.endswith(".pdf"):
+        return
 
-for q in questions:
-  print(call_openai(contents, q, name))
+    contents = read_resume(file_path)
+    name = call_openai(contents, 'what is the person name?', normal_string=True)
+
+    questions = [
+      'What is the person current company?',
+      'Which professors this person has worked with?',
+      'How many year of experience this person had?',
+      'Which school did this person go to?',
+      'What is the person title?',
+      'Where is the person located?'
+    ]
+
+    for q in questions:
+        data = call_openai(contents, q, name).replace("\\n", ""). replace("..", ".")
+        print(data)
+        file_writer.write(data)
+        file_writer.write("\n")
+
+
+def main():
+    dir_list = sorted(os.listdir('pdf'))
+    with open("fined_tune.txt", "w") as f:
+        for file_name in dir_list:
+            start_time = time.time()
+            extract_data(f"pdf/{file_name}", f)
+            print(f"{file_name} took: {time.time() - start_time}s")
+
+
+if __name__ == '__main__':
+    main()
